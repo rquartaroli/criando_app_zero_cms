5ae4f47b5bde2dcef312158d20736ff03e63a7bf
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2021-05-11 20:34:57 -0300

    Second commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import { GetStaticProps } from 'next';
 
 import Prismic from '@prismicio/client';
-import { RichText } from 'prismic-dom';
 import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+// eslint-disable-next-line import/order
+import { Head } from 'next/document';
 
 interface Post {
   uid?: string;
@@ -26,35 +27,53 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <Head>
+        <title>Desafio CMS</title>
+      </Head>
+      <div>
+        {/* {postsPagination.results.map(post => (
+          <h1>{post.data.title}</h1>
+        ))} */}
+        <main>
+          <h1>Hello World</h1>
+        </main>
+      </div>
+    </>
+  );
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient();
   const postsResponse = await prismic.query(
     [Prismic.predicates.at('document.type', 'repeatable')],
     {
-      fetch: ['repeatable.title', 'repeatable.content'],
+      fetch: ['repeatable.title', 'repeatable.subtitle', 'repeatable.author'],
       pageSize: 20,
     }
   );
 
   const posts = postsResponse.results.map(post => {
     return {
-      slug: post.uid,
-      title: RichText.asText(post.data.title),
-      excerpt:
-        post.data.content.find(content => content.type === 'paragraph')?.text ??
-        '',
-      updatedAt: new Date(post.last_publication_date).toLocaleDateString(
-        'pt-BR',
+      uid: post.uid,
+      first_publication_date: new Date(
+        post.last_publication_date
+      ).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric',
+      }),
+      data: [
         {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric',
-        }
-      ),
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        },
+      ],
     };
   });
 
--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,5 +1,8 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 
+import Prismic from '@prismicio/client';
+import { RichText } from 'prismic-dom';
+
 import { getPrismicClient } from '../../services/prismic';
 
 import commonStyles from '../../styles/common.module.scss';
@@ -30,16 +33,55 @@ interface PostProps {
 //   // TODO
 // }
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths: GetStaticPaths = async () => {
+  // const prismic = getPrismicClient();
+  // const posts = await prismic.query(
+  //   [Prismic.predicates.at('document.type', 'repeatable')],
+  //   {
+  //     fetch: ['repeatable.title', 'repeatable.subtitle', 'repeatable.author'],
+  //     pageSize: 20,
+  //   }
+  // );
 
-//   // TODO
-// };
+  return {
+    paths: [],
+    fallback: 'blocking',
+  };
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('repeatable', String(slug), {});
 
-//   // TODO
-// };
+  const post = {
+    first_publication_date: new Date(
+      response.last_publication_date
+    ).toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: 'long',
+      year: 'numeric',
+    }),
+    data: [
+      {
+        title: RichText.asText(response.data.title),
+        banner: {
+          url: RichText.asText(response.data.banner),
+        },
+        author: RichText.asText(response.data.author),
+        content: {
+          heading: RichText.asText(response.data.heading),
+          body: {
+            text: RichText.asText(response.data.body),
+          },
+        },
+      },
+    ],
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
